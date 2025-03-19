073cf02428c1014e1cedec2a3e55d8fd604c4da1
0GiS0 <giselatb@outlook.com>
2025-03-19 10:46:12 +0100

    Add initial implementation of mcp-remote server
    - Created package.json with dependencies and scripts for building and starting the server.
    - Implemented the main server logic in src/index.ts using Express and Model Context Protocol SDK.
    - Added TypeScript configuration in tsconfig.json for compiling the project.

---
+++ mcp-remote/src/index.ts
@@ -0,0 +1,69 @@
+import express from "express";
+import { McpServer, ResourceTemplate } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+import { z } from "zod";
+
+
+const PORT = process.env.PORT || 3001;
+let transport: SSEServerTransport;
+
+
+const server = new McpServer({
+    name: "remote-mcp-server",
+    version: "1.0.0"
+});
+
+server.resource("echo",
+    new ResourceTemplate("echo://{message}", { list: undefined }),
+    async (uri, { message }) => ({
+        contents: [{
+            uri: uri.href,
+            text: `Resource echo: ${message}`
+        }]
+    })
+);
+
+server.tool(
+    "echo",
+    "Echo a message",
+    { message: z.string() },
+    async ({ message }) => ({
+        content: [{ type: "text", text: `Tool echo: ${message}` }]
+    })
+);
+
+server.prompt(
+    "echo",
+    { message: z.string() },
+    ({ message }) => ({
+        messages: [{
+            role: "user",
+            content: {
+                type: "text",
+                text: `Please process this message: ${message}`
+            }
+        }]
+    })
+);
+
+const app = express();
+
+app.get("/sse", async (req, res) => {
+
+    console.log("Received SSE connection");
+
+    transport = new SSEServerTransport("/message", res);
+    await server.connect(transport);
+
+});
+
+app.post("/message", async (req, res) => {
+
+    console.log("Received message");
+
+    await transport.handlePostMessage(req, res);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
